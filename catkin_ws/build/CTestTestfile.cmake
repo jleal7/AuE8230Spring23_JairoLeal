f27ad48de71aa10c422a62eb397d49396dfacbd2
Jairo Leal <jairoandresleal99@gmail.com>
2023-02-03 14:45:09 -0500

    fixed folder structure so now each pkg inside src folder is an assignment and don't have to keep recreating catkin workspaces

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/jairo/Desktop/AuE8230Spring23_JairoLeal/catkin_ws/src
+# Build directory: /home/jairo/Desktop/AuE8230Spring23_JairoLeal/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
